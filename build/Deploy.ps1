a98027277d0e89bb73af149e3b0ce272d7fe6e7a
Tobias Persson <tobper@gmail.com>
2013-12-17 17:30:05 +0100

    Added sources to NuGet symbols package.

--- build/Deploy.ps1
+++ build/Deploy.ps1
@@ -11,5 +11,5 @@ Param (
     $projectBuildNumber
 )
 
-& $srcFolder\tools\NuGet.exe pack $srcFolder\src\ObjectCompare.nuspec -BasePath $tempFolder\$projectName -Version $projectVersion -OutputDirectory $outFolder -Symbols
+& $srcFolder\tools\NuGet.exe pack $srcFolder\src\ObjectCompare.nuspec -BasePath $buildFolder -Version $projectVersion -OutputDirectory $outFolder -Symbols
 & $srcFolder\tools\NuGet.exe push $outFolder\$projectName.$projectVersion.nupkg -ApiKey $variables["SecureNuGetApiKey"] -NonInteractive
